52934ad10dcd2b03cc684ee048e5a9cc22e3956c
Joffily F <joffily.ferreira@scielo.org>
2019-10-30 13:55:53 -0300

    Fixa versão do python slugify

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         "blinker>=1.4",
         "mongoengine>=0.13.0",
-        "python-slugify>=1.2.4",
+        "python-slugify==3.0.0",
         "legendarium>=2.0.1",
     ],
     tests_require=[
